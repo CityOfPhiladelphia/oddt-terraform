0e1bfbe1eca601a149077a903cc1e765045f29e0
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-02-24 15:13:53 -0500

    Planning out data engineering ecs cluster

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ provider "aws" {
 }
 
 #### TODO: dropbox S3 and permissions
+#### TODO: SSH tunnel gateway
+#### TODO: bastion server ?
+
 #### TODO: Domains?
 #### TODO: SSL?
 
@@ -16,7 +19,7 @@ resource "aws_vpc" "data_engineering" {
   cidr_block = "10.0.0.0/16"
 
   tags {
-      Name = "${var.name_prefix}"
+      Name = "${var.name_prefix}-vpc"
       Department = "${var.department}"
   }
 }
@@ -28,7 +31,7 @@ resource "aws_subnet" "data_engineering" {
   vpc_id            = "${aws_vpc.data_engineering.id}"
 
   tags {
-      Name = "${var.name_prefix}-${self.availability_zone}"
+      Name = "${var.name_prefix}-subnet-${self.availability_zone}"
       Department = "${var.department}"
   }
 }
@@ -37,7 +40,7 @@ resource "aws_internet_gateway" "data_engineering" {
   vpc_id = "${aws_vpc.data_engineering.id}"
 
   tags {
-      Name = "${var.name_prefix}"
+      Name = "${var.name_prefix}-gateway"
       Department = "${var.department}"
   }
 }
@@ -51,7 +54,7 @@ resource "aws_route_table" "data_engineering" {
   }
 
   tags {
-      Name = "${var.name_prefix}"
+      Name = "${var.name_prefix}-routing-table"
       Department = "${var.department}"
   }
 }
@@ -62,52 +65,404 @@ resource "aws_route_table_association" "data_routing_table_association" {
   route_table_id = "${aws_route_table.data_engineering.id}"
 
   tags {
-      Name = "${var.name_prefix}"
+      Name = "${var.name_prefix}-routing-table-association"
       Department = "${var.department}"
   }
 }
 
 ### Compute
 
-# TODO: Get ECS optimized AMI thorugh `data "aws_ami" ...`
+resource "aws_autoscaling_group" "data_engineering_cluster" {
+  name                 = "${var.name_prefix}-asg"
+  vpc_zone_identifier  = ["${aws_subnet.data_engineering.*.id}"]
+  min_size             = "${var.data_engineering_asg_min}"
+  max_size             = "${var.data_engineering_asg_max}"
+  desired_capacity     = "${var.data_engineering_asg_desired}"
+  launch_configuration = "${aws_launch_configuration.data_engineering_cluster.name}"
+}
 
-# TODO: add ECS agent config to instance user data?
+data "aws_ami" "ecs_optimized" {
+  most_recent = true
 
-# TODO: Create two EC2 instances in two AZs using `resource "aws_launch_configuration" ..`
+  filter {
+    name   = "name"
+    values = ["amzn-ami-*-amazon-ecs-optimized"]
+  }
 
-### Security
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 
-# TODO: Data VPC ELB security group `resource "aws_security_group" "data_elb_sg" { ...`
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-# TODO: ECS cluster instance security group
+  owners = ["591542846629"] # Amazon
+}
 
-## ECS
+# TODO: Add authorized_keys via user data?
 
-resource "aws_ecs_cluster" "data" {
-  name = "${var.name_prefix}" ## ECS cluster name, not name tag
+data "template_file" "data_engineering_ecs_user_data" {
+  template = "${file("${path.module}/user_data/ecs_service.tpl")}"
+
+  vars {
+    cluster_name = "${aws_ecs_cluster.data_engineering_cluster.name}"
+  }
+}
+
+resource "aws_launch_configuration" "data_engineering_cluster" {
+  security_groups = [
+    "${aws_security_group.data_engineering_instance_sg.id}",
+  ]
+
+  key_name                    = "${var.key_name}"
+  image_id                    = "${data.aws_ami.ecs_optimized.id}"
+  instance_type               = "${var.data_engineering_instance_type}"
+  iam_instance_profile        = "${aws_iam_instance_profile.data_engineering_cluster.name}"
+  user_data                   = "${data.template_file.data_engineering_ecs_user_data.rendered}"
+  associate_public_ip_address = true # TODO: remove once we create bastion in VPC? internet gateway?
+
+  lifecycle {
+    create_before_destroy = true # TODO: confirm this is needed
+  }
 
   tags {
-      Name = "${var.name_prefix}"
+      Name = "${var.name_prefix}-ecs-launch-config"
       Department = "${var.department}"
   }
 }
 
-# TODO: tasks for airflow-webserver and airflow-scheduler `data "template_file" "task_definition" {` .. `resource "aws_ecs_task_definition" "..." {`
+### Security
 
-# TODO: service for airflow-webserver and airflow-scheduler `resource "aws_ecs_service" ...` - airflow-scheduler should have max 1 instance and higher memory / CPU
+resource "aws_security_group" "data_engineering_elb_sg" {
+  vpc_id = "${aws_vpc.data_engineering.id}"
+  name   = "${var.name_prefix}-ecs-lbsg"
+
+  ingress { ### !!!!!! TODO: what do do here before SSL and auth?
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  tags {
+      Name = "${var.name_prefix}-ecs-elb-sg"
+      Department = "${var.department}"
+  }
+}
+
+resource "aws_security_group" "data_engineering_instance_sg" {
+  vpc_id      = "${aws_vpc.data_engineering.id}"
+  name        = "${var.name_prefix}-ecs-instance-sg"
+
+  ingress {
+    protocol  = "tcp"
+    from_port = 22
+    to_port   = 22
+
+    cidr_blocks = ["0.0.0.0/0"] ## TODO: lock this down more? Maybe after bastion
+  }
+
+  ingress {
+    protocol  = "tcp"
+    from_port = 8080
+    to_port   = 8080
+
+    security_groups = [
+      "${aws_security_group.data_engineering_elb_sg.id}",
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+      Name = "${var.name_prefix}-ecs-instance-sg"
+      Department = "${var.department}"
+  }
+}
+
+## ECS
+
+resource "aws_ecs_cluster" "data_engineering_cluster" {
+  name = "${var.name_prefix}-ecs-cluster" ## ECS cluster name, not name tag
+
+  tags {
+      Name = "${var.name_prefix}-ecs-cluster"
+      Department = "${var.department}"
+  }
+}
+
+# Airflow webserver
+
+data "template_file" "airflow_webserver_task_definition" {
+  template = "${file("${path.module}/task_definitions/airflow_webserver.json")}"
+
+  vars {
+    image_url        = "phila_airflow:latest" ## TODO: need to publish image first
+    container_name   = "airflow_webserver"
+    log_group_region = "${var.aws_region}"
+    log_group_name   = "${aws_cloudwatch_log_group.data_engineering_ecs_cluster.name}"
+  }
+}
+
+resource "aws_ecs_task_definition" "airflow_webserver_task_definition" {
+  family                = "${name_prefix}-airflow"
+  container_definitions = "${data.template_file.airflow_webserver_task_definition.rendered}"
+
+  tags {
+      Name = "${var.name_prefix}-ecs-cluster"
+      Department = "${var.department}"
+  }
+}
+
+resource "aws_ecs_service" "airflow_webserver" {
+  name            = "${name_prefix}-airflow-webserver"
+  cluster         = "${aws_ecs_cluster.data_engineering_cluster.id}"
+  task_definition = "${aws_ecs_task_definition.airflow_webserver_task_definition.arn}"
+  desired_count   = 2
+  iam_role        = "${aws_iam_role.ecs_service.name}"
+
+  load_balancer {
+    elb_name = "${aws_elb.airflow_webserver.id}"
+    container_name   = "airflow_webserver"
+    container_port   = "8080"
+  }
+
+  depends_on = [
+    "aws_iam_role_policy.ecs_service",
+    "aws_elb_listener.airflow",
+  ]
+
+  tags {
+      Name = "${var.name_prefix}-ecs-cluster"
+      Department = "${var.department}"
+  }
+}
+
+# Airflow scheduler
+
+data "template_file" "airflow_scheduler_task_definition" {
+  template = "${file("${path.module}/task_definitions/airflow_webserver.json")}"
+
+  vars {
+    image_url        = "phila_airflow:latest" ## TODO: need to publish image first
+    container_name   = "airflow_scheduler"
+    log_group_region = "${var.aws_region}"
+    log_group_name   = "${aws_cloudwatch_log_group.data_engineering_ecs_cluster.name}"
+  }
+}
+
+resource "aws_ecs_task_definition" "airflow_scheduler_task_definition" {
+  family                = "${name_prefix}-airflow"
+  container_definitions = "${data.template_file.airflow_scheduler_task_definition.rendered}"
+
+  tags {
+      Name = "${var.name_prefix}-ecs-cluster"
+      Department = "${var.department}"
+  }
+}
+
+# TODO: should other properties be used to keep the number of runnings tasks from going over one?
+
+resource "aws_ecs_service" "airflow_scheduler" {
+  name            = "${name_prefix}-airflow-scheduler"
+  cluster         = "${aws_ecs_cluster.data_engineering_cluster.id}"
+  task_definition = "${aws_ecs_task_definition.airflow_scheduler_task_definition.arn}"
+  desired_count   = 1
+  iam_role        = "${aws_iam_role.ecs_service.name}"
+
+  depends_on = [
+    "aws_iam_role_policy.ecs_service",
+  ]
+
+  tags {
+      Name = "${var.name_prefix}-ecs-cluster"
+      Department = "${var.department}"
+  }
+}
+
+# TODO: research placement strategies and constraints
 
 ## IAM
 
-# TODO: ecs service role and policy - used by ECS itself to create/alter resources
+resource "aws_iam_role" "ecs_service" {
+  name = "${name_prefix}-ecs-role"
 
-# TODO: ecs instance profile, role, and policy 
+  tags {
+      Name = "${var.name_prefix}-ecs-cluster"
+      Department = "${var.department}"
+  }
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs_service" {
+  name = "${name_prefix}-ecs-policy"
+  role = "${aws_iam_role.ecs_service.name}"
+
+  tags {
+      Name = "${var.name_prefix}-ecs-cluster"
+      Department = "${var.department}"
+  }
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:Describe*",
+        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+        "elasticloadbalancing:DeregisterTargets",
+        "elasticloadbalancing:Describe*",
+        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+        "elasticloadbalancing:RegisterTargets"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "data_engineering_cluster" {
+  name  = "${name_prefix}-ecs-instance-profile"
+  roles = ["${aws_iam_role.data_engineering_cluster_instance.name}"]
+
+  tags {
+      Name = "${var.name_prefix}-ecs-cluster"
+      Department = "${var.department}"
+  }
+}
+
+resource "aws_iam_role" "data_engineering_cluster_instance" {
+  name = "${name_prefix}-ecs-instance-role"
+
+  tags {
+      Name = "${var.name_prefix}-ecs-cluster"
+      Department = "${var.department}"
+  }
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+data "template_file" "data_engineering_cluster_instance_profile" {
+  template = "${file("${path.module}/policies/data_engineering_ecs_instance_profile_policy.json")}"
+
+  vars {
+    container_log_group_arn = "${aws_cloudwatch_log_group.container.arn}"
+    ecs_log_group_arn = "${aws_cloudwatch_log_group.ecs.arn}"
+  }
+}
+
+resource "aws_iam_role_policy" "data_engineering_cluster_instance" {
+  name   = "TfEcsExampleInstanceRole"
+  role   = "${aws_iam_role.app_instance.name}"
+  policy = "${data.template_file.instance_profile.rendered}"
+
+  tags {
+      Name = "${var.name_prefix}-ecs-cluster"
+      Department = "${var.department}"
+  }
+}
 
 ## ELB
 
-# TODO: Create ELB for airflow-webserver
+resource "aws_elb" "airflow_webserver" {
+  name = "${var.name_prefix}-airflow-webserver"
+  subnets         = ["${aws_subnet.data_engineering.*.id}"]
+  security_groups = ["${aws_security_group.data_engineering_elb_sg.id}"]
+
+  listener {
+    instance_port = 8080
+    instance_protocol = "http"
+    lb_port = 80 ## TODO: ssl
+    lb_protocol = "http"
+  }
+
+  health_check { ## !!!!! TODO: double check this path with airflow webserver
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:8080/"
+    interval = 30
+  }
+
+  cross_zone_load_balancing = true
+  idle_timeout = 400
+  connection_draining = true
+  connection_draining_timeout = 400
+
+  tags {
+      Name = "${var.name_prefix}-airflow-webserver-elb"
+      Department = "${var.department}"
+  }
+}
 
 ## CloudWatch Logs
 
 # TODO: group for ecs itself? - Can we pass this without creating the agent ourselves?
 
-# TODO: group for tasks?
+resource "aws_cloudwatch_log_group" "ecs" {
+  name = "${var.name_prefix}/ecs-agent"
+
+  tags {
+      Name = "${var.name_prefix}-logs-ecs-agent"
+      Department = "${var.department}"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "container" {
+  name = "${var.name_prefix}/ecs-containers"
+
+  tags {
+      Name = "${var.name_prefix}-ecs-containers"
+      Department = "${var.department}"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
 variable "name_prefix" {
+  description = "A string of text to place before each Name tag"
   default     = "oddt-data-engineering"
 }
 
 variable "department" {
+  description = "City / organizational department"
   default     = "oddt"
 }
 
@@ -10,7 +12,32 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "key_name" {
+  description = "RSA key pair to place on created instances"
+  default     = "andrew.madonna"
+}
+
 variable "az_count" {
   description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
+}
+
+variable "data_engineering_instance_type" {
+  description = "Instance type to use for the data engineering service cluster"
+  default     = "t2.medium"
+}
+
+variable "data_engineering_asg_min" {
+  description = "Minium number of instaces for the data engineering Docker cluster auto scaling group"
+  default     = "1"
+}
+
+variable "data_engineering_asg_max" {
+  description = "Maximum number of instaces for the data engineering Docker cluster auto scaling group"
+  default     = "3"
+}
+
+variable "data_engineering_asg_desired" {
+  description = "Desired number of instaces for the data engineering Docker cluster auto scaling group"
   default     = "2"
 }
